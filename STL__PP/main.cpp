d468974422d2f5c23e1a76f03658087f9d60f402
Aleksandr Petushkov <aleks.petushkov@gmail.com>
2015-12-07 16:51:26 +0600

    begin v 0.1.0

---
+++ STL__PP/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+
+using namespace std;
+
+
+template<class K> ostream & operator<<(ostream &output, vector<K> &val)
+{
+	int i(1);
+	for (vector<K>::iterator iter = val.begin(); iter != val.end(); ++iter, ++i)
+	{
+		output << i << " element - " << (*iter) << endl;
+	}
+	return output;
+}
+
+template<class K, class T> ostream & operator<<(ostream &output, map<K, T> &val) 
+{
+	int i(1);
+	for (map<K, T>::iterator iter = val.begin(); iter != val.end(); ++iter, ++i)
+	{
+		output <<i<<".element - " "key: " << iter->first << " value: " << iter->second<<endl;
+	}
+	return output;
+}
+
+template <class K> ostream & operator<<(ostream &output, set<K> val)
+{
+	int i(1);
+	for (set<K>::iterator iter = val.begin(); iter != val.end(); ++iter)
+	{
+		output << i << " element - " << (*iter) << endl;
+	}
+	return output;
+}
+
+int main()
+{
+	cout << "vector:" << endl;
+	vector<int> V;
+	V.push_back(11);
+	cout << V << endl;
+
+	cout << "map:" << endl;
+	map<int, int> M;
+	M[1] = 2;
+	cout << M << endl;
+
+	cout << "set:" << endl;
+	set<int> s;
+	s.insert(11);
+	cout << s << endl;
+	
+	cin.get();
+}
